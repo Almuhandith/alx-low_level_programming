ae3b0cf2b722229c40f0b40b24f5ac4da244b6ef
Abdulsamad <almuhandith1497@gmail.com>
2022-09-09 13:30:40 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Print first 10 numbers of base 10
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(num[i]);
+	}
+	putchar('\n');
+	return (0);
+}
