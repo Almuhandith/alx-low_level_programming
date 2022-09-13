cb9a0eadd8fc30a2f37994192579c9217f0beb7a
Abdulsamad <almuhandith1497@gmail.com>
2022-09-13 12:56:02 -0700

    Prints alphabets

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - prints 1 for letters and 0 for others
+ *
+ * Return: 1 for letters, 0 for others
+ */
+
+int _isalpha(int c)
+{
+	if ((c > 96 && c < 123) || (c > 64 && c < 91))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
