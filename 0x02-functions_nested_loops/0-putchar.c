f9f1114ec1beb930848e2134f1738c4ea3d5a24f
Abdulsamad <almuhandith1497@gmail.com>
2022-09-13 10:53:52 -0700

    Print Alphabet

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,13 @@
  */
 int main(void)
 {
-	int ptc[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int array[] = {95, 112, 117, 116, 99, 104, 97, 114};
 	int spell, size;
 
-	size = sizeof(ptc) / sizeof(int);
+	size = sizeof(array) / sizeof(int);
 	for (spell = 0; spell < size; spell++)
 	{
-		_putchar(ptc[spell]);
+		_putchar(array[spell]);
 	}
 	_putchar('\n');
 	return (0);
