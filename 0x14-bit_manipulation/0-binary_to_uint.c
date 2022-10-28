3e7d5393a91231f38eadb45fadd3aa269ee75e76
Abdulsamad <almuhandith1497@gmail.com>
2022-10-28 13:21:32 -0700

    fixing task 2

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,17 +17,17 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 
-	while (b)
+	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		else
-		{
-			val <<= 1;
-			val += b[i] - '0';
-			i++;
-		}
 	}
-	printf("%i\n", val);
+
+	while (b[i] == '0' || b[i] == '1')
+	{
+		val <<= 1;
+		val += b[i] - '0';
+		i++;
+	}
 	return (val);
 }
