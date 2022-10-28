9e8c5c3a740cb5b3dd4407964408379b81bcf6da
Abdulsamad <almuhandith1497@gmail.com>
2022-10-28 13:38:45 -0700

    fixing: task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,19 +10,12 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int i;
+	unsigned long int i;
 
-	if (n == 0 && index < 64)
-		return (0);
+	if (index > 64)
+		return (-1);
 
-	for (i = 0; i <= 63; n >>= 1, i++)
-	{
-		if (index == i)
-		{
-			return (n & 1);
-		}
-	}
+	i = n >> index;
 
-
-	return (-1);
+	return (i & 1);
 }
