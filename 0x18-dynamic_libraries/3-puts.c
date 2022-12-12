299c4dd9843cba575d99e477eed7002609dea990
Abdulsamad <almuhandith1497@gmail.com>
2022-12-12 17:27:27 +0100

    dynamic libraries

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a new line
+ * @str: input str
+ * Return: no return
+ */
+void _puts(char *s)
+{
+	int i = 0;
+
+	while (i >= 0)
+	{
+		if (s[i] == '\0')
+		{
+			break;
+		}
+		i++;
+	}
+}
